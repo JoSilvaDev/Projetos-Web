115d9db52a8f2c6ef60da1b15f5d7e34f0f8ebb4
JoSilvaDev <joilsons175@gmail.com>
2022-10-13 20:39:28 -0300

    up

--- Salão/script/script.js
+++ Salão/script/script.js
@@ -1,6 +1,7 @@
 function mudouTamanho(){
     if(window.innerWidth >= 768){
         menu.style.display = 'block'
+        topo.style.height = '120px'
     }else{
         menu.style.display = 'none'
     }
@@ -9,7 +10,7 @@ function mudouTamanho(){
 function clickMenu(){
     if(menu.style.display == 'block' ){
         menu.style.display = 'none'
-        topo.style.height = '100px'
+        topo.style.height = '120px'
     }else{
         menu.style.display = 'block'
         topo.style.height = '150px'
