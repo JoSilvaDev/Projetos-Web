02c4960506ae827516df7175038490a8bec4474f
JoSilvaDev <joilsons175@gmail.com>
2022-10-13 20:34:01 -0300

    menu upgrade

--- Salão/script/script.js
+++ Salão/script/script.js
@@ -1,7 +1,17 @@
+function mudouTamanho(){
+    if(window.innerWidth >= 768){
+        menu.style.display = 'block'
+    }else{
+        menu.style.display = 'none'
+    }
+}
+
 function clickMenu(){
     if(menu.style.display == 'block' ){
         menu.style.display = 'none'
+        topo.style.height = '100px'
     }else{
         menu.style.display = 'block'
+        topo.style.height = '150px'
     }
 }
