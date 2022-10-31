e8a2a6af14d97166054b80666214d2dd86cd3223
JoSilvaDev <joilsons175@gmail.com>
2022-10-31 15:56:08 -0300

    up

---
+++ xbox-landing-page/script/script.js
@@ -0,0 +1,29 @@
+function clickMenu() {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function (element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass)
+        } else {
+            element.classList.remove(animationClass)
+        }
+    })
+}
+
+animeScroll();
+
+if (target.length) {
+    window.addEventListener('scroll', function () {
+        animeScroll();
+    })
+}
